e1764815420fa496a55d309fb821e12441744669
sarathmohan95 <sarathkumar.mohan6@gmail.com>
2017-08-11 18:50:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById('my-id');
 element.innerHTML ='new text';
 
-var img = document.getElementById('dino')
+var img = document.getElementById('dino');
 img.click= function() {
 img.style.marginRight='100px';
 };

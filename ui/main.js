f4c62e540322b3fdda7132d52a1198aefef0d25c
sarathmohan95 <sarathkumar.mohan6@gmail.com>
2017-08-11 18:40:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element = document.getelementbyid('my-id');
-element.innerhtml('new text');
+var element = document.getElementById('my-id');
+element.innerHTML ='new text';
